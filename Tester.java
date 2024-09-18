a4317437a7f8df618a155293935c0ecdef7f776c
Tyler <tanderson1@hwemail.com>
2024-09-18 00:32:35 -0700

    Added Javadoc comments to certain helper methods

--- Tester.java
+++ Tester.java
@@ -1,9 +1,23 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class Tester {
-    public static void main(String[] args) throws IOException {
-        testInitRepo();
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        
+    }
+
+    //not yet working
+    public static void testCreateBlob() throws IOException, NoSuchAlgorithmException {
+        //creates a file with a random name and a random amount of data
+        File file = new File(randomString(5));
+        file.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(randomString(randomInt(0, 100)));
+        writer.close();
+        Git.createBlob(file.getPath());
     }
 
     public static void testInitRepo() throws IOException {
@@ -46,17 +60,31 @@ public class Tester {
         return gitFolder.exists() && objectFolder.exists() && indexFile.exists();
     }
 
-    // Generates a random string of length 1-50
-    private static String randomString() {
+    /**
+     * @param length - the length of the string
+     * @return a string of random lower case letters
+     */
+    private static String randomString(int length) {
         StringBuilder name = new StringBuilder();
-        for (int i = 0; i < (int) (Math.random() * 50 + 1); i++) {
+        for (int i = 0; i < length; i++) {
             name.append(randomChar());
         }
         return name.toString();
     }
 
-    // Generates a random lowercase character
+    /**
+     * @return a random lower case character
+     */
     private static char randomChar() {
         return (char) (int) (Math.random() * (122 - 97) + 97);
+    }
+     
+    /**
+     * @param low - the low value
+     * @param high - the high value
+     * @return a random int between low and high: inclusive, exclusive
+     */
+    private static int randomInt (int low, int high) {
+        return (int) (Math.random() * (high - low) + low);
     }
 }
