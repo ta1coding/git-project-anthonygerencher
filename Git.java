55f3f0f166eefa1267f606370b79f9dfed9aff5a
Tyler <tanderson1@hwemail.com>
2024-09-17 22:15:48 -0700

    Fixed initRepo and renamed to initRepoHere
    Changed the initRepo function to initiate the repo in the folder the directory that Git.java is in, instead of passing in the directory in which to initiate the repo. Renamed the method accordingly

--- Git.java
+++ Git.java
@@ -6,30 +6,27 @@ public class Git {
 
     }
 
-    public static void initRepo(String directoryName) throws IOException {
+    public static void initRepoHere() throws IOException {
 
         // Initiate requisite files
-        File directory = new File(directoryName);
-        File gitFolder = new File(directoryName + "/git");
-        File objectFolder = new File(directoryName + "/git/objects");
-        File indexFile = new File(directoryName + "/git/index");
+        File gitFolder = new File("git");
+        File objectFolder = new File("git/objects");
+        File indexFile = new File("git/index");
 
         //Checks if repository has already been created
-        // if (directory.exists() && gitFolder.exists() && objectFolder.exists() && indexFile.exists()) {
-        //     System.out.println("Git Repository already exists");
-        // } 
+        if (gitFolder.exists() && objectFolder.exists() && indexFile.exists()) {
+            System.out.println("Git Repository already exists");
+        } 
 
         // Create requisite directories and files if they don't already exist
-        // else {
-            if (!directory.exists())
-                directory.mkdir();
+        else {
             if (!gitFolder.exists())
                 gitFolder.mkdir();
             if (!objectFolder.exists())
                 objectFolder.mkdirs();
             if (!indexFile.exists())
                 indexFile.createNewFile();
-        // }
+        }
 
     }
 }
