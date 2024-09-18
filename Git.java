36e6da5695ef1a97efae5c5e103f2689fc351ab8
Tyler <tanderson1@hwemail.com>
2024-09-18 00:04:23 -0700

    Added updateIndex method
    Added a method to update the index with a file name and hash pair

--- Git.java
+++ Git.java
@@ -1,5 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,7 +10,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Git {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        createBackup("test");
+        updateIndex("test", generateHash("test"));
     }
 
     // Creates the requisite files and directories for a repository in this folder
@@ -34,21 +36,32 @@ public class Git {
         }
     }
 
-    //Not yet working
+    // Not yet working
     public static void createBlobHere(String pathToFile) throws NoSuchAlgorithmException, IOException {
         String hash = generateHash(pathToFile);
 
+        // should compress the file first for that sweet sweet S+ super credit
+        createBackup(pathToFile, hash);
+        updateIndex(pathToFile, hash);
     }
 
-    //copies the contents of the original file to the backup
-    private static void createBackup (String pathToFile) throws IOException, NoSuchAlgorithmException {
-        String hash = generateHash(pathToFile);
+    //Updates the index with the file name and hash
+    private static void updateIndex(String pathToFile, String hash) throws IOException {
+        File index = new File("git/index");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(index, true));
+        writer.write(hash + " " + pathToFile.substring(pathToFile.lastIndexOf("/") + 1));
+        writer.newLine();
+        writer.close();
+    }
+
+    // copies the contents of the original file to the backup
+    private static void createBackup(String pathToFile, String hash) throws IOException, NoSuchAlgorithmException {
         Path source = Path.of(pathToFile);
         Path destination = Path.of("git", "objects", hash);
         Files.copy(source, destination);
     }
 
-    //Generates the hash for a file based on the data in the file
+    // Generates the hash for a file based on the data in the file
     private static String generateHash(String pathToFile) throws NoSuchAlgorithmException, IOException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         File file = new File(pathToFile);
@@ -67,7 +80,7 @@ public class Git {
         return byteArrayToHexString(hash);
     }
 
-    //turns a byte array into a hexidecimal number stored in String form
+    // turns a byte array into a hexidecimal number stored in String form
     private static String byteArrayToHexString(byte[] array) {
         StringBuilder string = new StringBuilder();
         for (byte b : array) {
@@ -76,7 +89,7 @@ public class Git {
         return string.toString();
     }
 
-    //Prints an array of bytes to the console
+    // Prints an array of bytes to the console
     private static void printByteArray(byte[] array) {
         for (byte b : array) {
             System.out.print(b + ", ");
