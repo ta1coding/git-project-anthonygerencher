61b637de8246b7756a8a1f173d31dde4fea6a827
Tyler <tanderson1@hwemail.com>
2024-09-18 01:29:44 -0700

    Added testCreateBlob method
    Added the tester to verify that the testCreateBlob method works.

--- Tester.java
+++ Tester.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 
 public class Tester {
@@ -9,23 +14,95 @@ public class Tester {
         testCreateBlob();
     }
 
-    // not yet working
-    public static void testCreateBlob() throws IOException, NoSuchAlgorithmException {
-        String fileName = randomString(5);
-        String fileData = randomString(randomInt(0, 100));
-        generateTestBlob(fileName, fileData);
 
+    public static void testCreateBlob() throws IOException, NoSuchAlgorithmException {
+        for (int i = 0; i < 100; i++) {
+            //creates a copy of the index
+            File index = new File("git/index");
+            Files.copy(Path.of("git", "index"), Path.of("git", "index_copy"));
+            File indexCopy = new File("git/index_copy");
+
+            //conducts the test
+            String fileName = randomString(5);
+            String fileData = randomString(randomInt(0, 100));
+            generateTestBlob(fileName, fileData);
+            if (!blobMatchesData(fileName, fileData)) {
+                System.out.println("createBlob method failed the test.");
+                return;
+            }
+
+            //cleans up
+            String hash = getHashFromIndex(fileName, getIndex());
+            File backupFile = new File("git/objects/" + hash);
+            File original = new File(fileName);
+            backupFile.delete();
+            original.delete();
+            index.delete();
+            indexCopy.renameTo(index);
+        }
+        System.out.println("createBlob method passed the test.");
     }
 
-    
+    private static boolean blobMatchesData(String fileName, String fileData) throws IOException {
+        String index = getIndex();
+
+        String hash = getHashFromIndex(fileName, index);
+
+        // verifies that the content of the backup matches the original data
+        File backup = new File("git/objects/" + hash);
+        byte[] fileByteData = fileData.getBytes("UTF-8");
+        FileInputStream input = new FileInputStream(backup);
+        byte[] backupFileData = new byte[(int) backup.length()];
+        input.read(backupFileData);
+        input.close();
+
+        return byteArraysAreTheSame(fileByteData, backupFileData);
+    }
+
+    /**
+     * @return the index in string form
+     * @throws IOException
+     */
+    private static String getIndex () throws IOException {
+        StringBuilder string = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader("git/index"));
+        while (reader.ready())
+            string.append(reader.readLine());
+        reader.close();
+        return string.toString();
+    }
+
+    /**
+     * @param fileName - the name of the file
+     * @param index - the index in string form
+     * @return the relevant hash based on the filename
+     */
+    private static String getHashFromIndex (String fileName, String index) {
+        int hashStartingIndex = index.indexOf(fileName) - 41;
+        int hashEndingIndex = hashStartingIndex + 40;
+        return index.substring(hashStartingIndex, hashEndingIndex);
+    }
+
+    private static boolean byteArraysAreTheSame (byte[] one, byte[] two) {
+        if (one.length != two.length)
+            return false;
+        for (int i = 0; i < one.length; i++) {
+            if (one[i] != two[i])
+                return false;
+        }
+        return true;
+    }
+
     /**
      * Creates a blob & updates the index based on the file name and data
+     * 
      * @param fileName - the name of the file
      * @param fileData - the data in the file
      * @throws IOException
      * @throws NoSuchAlgorithmException
      */
-    private static void generateTestBlob(String fileName, String fileData) throws IOException, NoSuchAlgorithmException {
+    private static void generateTestBlob(String fileName, String fileData)
+            throws IOException, NoSuchAlgorithmException {
         File file = new File(fileName);
         file.createNewFile();
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
