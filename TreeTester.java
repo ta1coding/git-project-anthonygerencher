866f8a9d72b57bf932ba6ed64205f6eeeb76b9f1
Tyler <tanderson1@hwemail.com>
2024-09-18 17:52:11 -0700

    Updated Tree Tester

--- TreeTester.java
+++ TreeTester.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class TreeTester {
-    public static void main(String[] args) throws IOException {
-        Tree.makeTreeFileHere();
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Tree.addFileToTree("test");
     }
 }
